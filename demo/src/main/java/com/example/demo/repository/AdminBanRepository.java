f069ae776430c7320d854957baede47b588f9292
neojarvis <techadmin@iamneo.ai>
2025-07-10 07:24:25 +0000

    Repositories

--- demo/src/main/java/com/example/demo/repository/AdminBanRepository.java
+++ demo/src/main/java/com/example/demo/repository/AdminBanRepository.java
@@ -1,5 +1,14 @@
 package com.example.demo.repository;
 
-public interface AdminBanRepository {
+import com.example.demo.entity.AdminBan;
+import com.example.demo.entity.User;
+import com.example.demo.entity.AdminBan.EntityType;
+import com.example.demo.entity.AdminBan.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+public interface AdminBanRepository extends JpaRepository<AdminBan, Integer> {
+    List<AdminBan> findByEntityTypeAndEntityId(EntityType entityType, Integer entityId);
+    List<AdminBan> findByStatus(Status status);
+    List<AdminBan> findByBannedBy(User bannedBy);
 }
